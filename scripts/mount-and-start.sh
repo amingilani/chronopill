dc67a73f53846d88699e2fdc9e2557070379c93f
amingilani <amin@gilani.me>
2017-07-09 22:29:00 +0500

    Finally get the app working

--- scripts/mount-and-start.sh
+++ scripts/mount-and-start.sh
@@ -7,11 +7,11 @@ echo "/dev/sda2 /media/tm hfsplus force,rw,user,auto 0 0" >> /etc/fstab
 # mount it all
 mount -a
 
-systemctl enable avahi-daemon
-systemctl enable netatalk
-
-
 # start services
 service dbus start
 service avahi-daemon start
-netatalk start
+service netatalk start
+
+# persist service on startup (not that resin needs this)
+systemctl enable avahi-daemon
+systemctl enable netatalk
