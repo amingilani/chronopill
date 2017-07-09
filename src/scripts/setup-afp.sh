40991d0d08baad36f407461dff21832ae12dbba6
amingilani <amin@gilani.me>
2017-07-10 01:23:00 +0500

    Add default user for timemachine

--- src/scripts/setup-afp.sh
+++ src/scripts/setup-afp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# setup configuration
 sed -i '/^hosts:/ s/$/ mdns4 mdns/' /etc/nsswitch.conf
 cp /src/afpd.service /etc/avahi/services/afpd.service
 cp /src/afp.conf /usr/local/etc/afp.conf
+
+# add new user
+useradd ${USERNAME:-'tmuser'}
+echo ${USERNAME:-'tmuser'}:${PASSWORD:-'password'} | chpasswd
+sed -i "/^  valid users =/ s/$/ ${USERNAME:-tmuser}/" /usr/local/etc/afp.conf
--- src/scripts/setup-ssh.sh
+++ src/scripts/setup-ssh.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-mkdir /var/run/sshd \
-    && echo 'root:resin' | chpasswd \
-    && sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
-    && sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
-
 /usr/sbin/sshd -p 22
